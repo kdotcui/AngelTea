d38db0c87b30e4a3af1d80e712de5aed7ff3860e
Kevin Cui <106274250+kdotcui@users.noreply.github.com>
2025-09-28 19:37:20 -0400

    quik fix

--- app/(site)/shop/page.tsx
+++ app/(site)/shop/page.tsx
@@ -3,6 +3,7 @@ import { useState} from 'react';
 import { ShopItem } from './types';
 import ItemPreviewCard from './ItemPreviewCard';
 import ItemPreviewModal from './ItemPreviewModal';
+// TODO: Remove this and replace with items stored in firebase.
 import { products } from './hardcodedProducts';
 
 export default function ShopPage() {
@@ -10,7 +11,7 @@ export default function ShopPage() {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold text-center mb-8">All Items</h1>
+      <h1 className="text-3xl font-bold text-center mb-8">Angel Tea Merchandise</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
         {products.map((product) => (
           <ItemPreviewCard
